030c0d845d82551c71b8ee6a0aa5ace9ebc944e4
Paulo Guachizaca <yosnap@gmail.com>
2022-07-28 01:28:29 +0200

    morgan

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ export const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 app.use(cors());
 
 app.use('/',api);
